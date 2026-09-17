783baaf84a076b2805c7e326c9fbe55c54712617
Nirav Patel <nirav4273@gmail.com>
2026-09-17 11:36:51 +0530

    Add multi-agent with conditional mapping in node

--- main.py
+++ main.py
@@ -1,54 +1,126 @@
 from langgraph.graph import StateGraph, START, END
-from pydantic import BaseModel, SecretStr
-from typing import List, Annotated
+from pydantic import BaseModel, Field, SecretStr
+from typing import Literal, cast
 from langchain_groq import ChatGroq
-from langgraph.graph.message import add_messages
-from langgraph.checkpoint.memory import InMemorySaver
+from langchain.agents import create_agent
+from langchain.tools import tool
 from dotenv import load_dotenv
 import os
+from langchain_tavily import TavilySearch
+
+
+CATEGORY_LITERAL = Literal['weather', 'coding', 'google_search',]
 
 load_dotenv()
 
-groq_api_key = SecretStr(os.getenv('GROQ_API_KEY', ''))
-if not groq_api_key:
-    raise RuntimeError('Set GROQ_API_KEY first: $env:GROQ_API_KEY="your_api_key"')
+class UserQuestionFlow(BaseModel):
+    question: str = Field("Question which asked by the user")
+    category: CATEGORY_LITERAL = Field(default="google_search", description="Question category will be store here and not match any category set google_search those kind of questions")
+    answer: str = Field(default="", description="Asked question result will be store inside this.")
 
-groq_model = os.getenv('GROQ_MODEL', '')
+class QuestionCategory(BaseModel):
+    category: CATEGORY_LITERAL = Field(default="google_search", description="Question category will be store here and not match any category set google_search those kind of questions")
 
-class ChatMessage(BaseModel):
-    messages: Annotated[list, add_messages]
 
-graph = StateGraph(ChatMessage)
 
-memory = InMemorySaver()
 llm = ChatGroq(
-    model=groq_model,
-    api_key=groq_api_key
+    api_key=SecretStr(os.getenv('GROQ_API_KEY', '')),
+    model= str(os.getenv('GROQ_MODEL', ''))
 )
 
-def ChatBotNode(state: ChatMessage) -> ChatMessage:
-    result = llm.invoke(state.messages)
-    state.messages = [result]
+def question_category(state: UserQuestionFlow) -> UserQuestionFlow:
+    llm_structured_output = llm.with_structured_output(QuestionCategory)
+
+    result = llm_structured_output.invoke(f"""
+    I want to know the question category of my question.
+    My Question is {state.question}
+    """)
+
+    result = cast(QuestionCategory, result);
+    state.category = result.category
     return state
 
-graph.add_node('chatBot', ChatBotNode)
+result =  question_category(UserQuestionFlow(question="What is 2+2?"))
 
-graph.add_edge(START, 'chatBot')
-graph.add_edge('chatBot', END)
+def route(state: UserQuestionFlow) -> CATEGORY_LITERAL:
+    return state.category
 
-final_graph = graph.compile(checkpointer=memory)
 
-while True:
-    query = input("Ask : ")
-    if query:
-        response = final_graph.invoke(ChatMessage(messages=[{
-            'role': 'human',
-            'content': query
-        }]), {
-            'configurable': {
-                'thread_id': 'test'
+tavily_search_tool  = TavilySearch(
+    max_results=5,
+    # topic="general",
+    # include_answer=False,
+    include_raw_content=False,
+    # include_images=False,
+    # include_image_descriptions=False,
+    search_depth="basic",
+    time_range="day",
+    # include_domains=None,
+    # exclude_domains=None
+)
+
+agent = create_agent(
+    model=llm,
+    tools=[tavily_search_tool],
+    system_prompt="You are web search assistant and get user ans for the asked question in short descriptions"
+)
+
+
+def google_search(state: UserQuestionFlow) -> UserQuestionFlow:
+    print("Google search Invoke")
+    result = agent.invoke({
+        'messages': [
+            {
+                'role': 'user',
+                'content': state.question
             }
-        })
+        ]
+    })
+    messages = result['messages']
+    state.answer = messages[-1].content
+    return state
 
-        for message in response['messages']:
-            print(">>>>", type(message) ,">>>> ",message.content)
+
+def coding(state: UserQuestionFlow) -> UserQuestionFlow:
+    result = llm.invoke(state.question)
+    state.answer = str(result.content)
+    return state
+
+def weather(state: UserQuestionFlow) -> UserQuestionFlow:
+    result = agent.invoke({
+        'messages': [
+            {
+                'role': 'user',
+                'content': state.question
+            }
+        ]
+    })
+    messages = result['messages']
+    state.answer = messages[-1].content
+    return state
+
+
+graph = StateGraph(UserQuestionFlow)
+graph.add_node('question_category', question_category)
+
+## ['weather', 'coding', 'google_search',]
+
+## Node name should be same as literal
+graph.add_node('coding',coding)
+graph.add_node('weather', weather)
+graph.add_node('google_search', google_search)
+
+graph.add_edge(START, 'question_category')
+graph.add_conditional_edges('question_category', route)
+graph.add_edge('coding', END)
+graph.add_edge('weather', END)
+graph.add_edge('google_search', END)
+
+final_graph = graph.compile()
+
+result = final_graph.invoke(UserQuestionFlow(
+    question= 'what is js'
+))
+print(result['category'])
+print(result['answer'])
+
